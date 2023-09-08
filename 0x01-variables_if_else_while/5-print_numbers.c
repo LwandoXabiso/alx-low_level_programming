4b96b0c400d6af56c62cea17ae2419a79a84572f
LwandoXabiso <lxb.sigalelana@gmail.com>
2023-09-08 12:27:35 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Programm Entry point
+ * Return: Always return 0 when successful
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	putchar('\n');
+	return (0);
+}

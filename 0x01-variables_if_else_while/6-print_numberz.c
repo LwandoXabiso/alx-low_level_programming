2402ca551014b8c31e989332b8ac9a842af1d181
LwandoXabiso <lxb.sigalelana@gmail.com>
2023-09-08 12:33:15 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Programm for Entry point
+ * Return: Always return 0 when successful
+ */
+
+int main(void)
+{
+	int c;
+
+	c = 0;
+	while
+		(c < 10) {
+			putchar(c + '0');
+		}
+	putchar('\n');
+	return (0);
+}

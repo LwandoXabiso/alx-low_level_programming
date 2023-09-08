24688f570c0c7203e69dcd7cad69eb49a4f5244c
LwandoXabiso <lxb.sigalelana@gmail.com>
2023-09-08 13:06:03 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Programm for Entry point
+ * Return: Always returns 0 when successful
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++) {
+		putchar(a + '0');
+ 		if (a < 9) {
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}

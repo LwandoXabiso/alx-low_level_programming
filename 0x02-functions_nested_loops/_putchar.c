98b23dc739f64605dc446b9e3569d304ca002f78
LwandoXabiso <lxb.sigalelana@gmail.com>
2023-09-12 12:01:21 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * _putchar - write the character c to stdio
+ * 0C! The character to print
+ * Return: 1 on success
+ * On error, -1 is returned, and error is set appropriately
+ */
+
+int _putchar(char c)
+
+{
+	return (write(1, &c, 1));
+}

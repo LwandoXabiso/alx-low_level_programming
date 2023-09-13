46b8e1aba0c86c07661c5eafb9a65df35c88b2d2
LwandoXabiso <lxb.sigalelana@gmail.com>
2023-09-13 12:54:28 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,23 @@
 int _putchar(char c);
+
+void print_alphabet(void);
+
+void print_alphabet_1x0(void);
+
+int _islower(int c);
+
+int _isalphabet(int c);
+
+int print_sign(int n);
+
+int _abs(int);
+
+int print_last_digit(int);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int, n);

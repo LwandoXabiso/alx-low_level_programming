021d6fb610583d3725aacc8e4e077f69e9b209e9
LwandoXabiso <lxb.sigalelana@gmail.com>
2023-09-13 15:51:16 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,7 +16,7 @@ void jack_bauer(void)
 		if (i < 10)
 		{
 			_putchar('0');
-			_putchar(i +'0');
+			_putchar(i + '0');
 		}
 		else if (i >= 10)
 		{
@@ -27,7 +27,7 @@ void jack_bauer(void)
 		{
 			_putchar(':');
 			_putchar('0');
-			_putchar(j);
+			_putchar(j + '0');
 		}
 		else if (j >= 10)
 		{

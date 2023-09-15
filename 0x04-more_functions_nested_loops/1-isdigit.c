156e4c7b87cfd0ea1dd71b454a17a9fb575a72cc
LwandoXabiso <lxb.sigalelana@gmail.com>
2023-09-15 15:17:34 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -6,11 +6,6 @@
  * Return: 1 for a character that will be a digit or 0 for anything else
  */
 
-int _isdigit(int c);
+int _isdigit(int c)
 {
-	if (c >= 48 && c <= 57)
-	{
-		return (1);
-	}
-	return (0);
-}
+	return ( c <= '0' && c >= '9');

ba9eb33451fd9299f17b1f50dd1a7d9869161146
LwandoXabiso <lxb.sigalelana@gmail.com>
2023-09-15 17:08:47 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,11 +7,17 @@
 
 void print_line(int n)
 {
-	int x;
+	int count = 0;
 
 	if (n <= 0)
 		_putchar('\n');
-	for (x = 0; x <= n; x++)
-		_putchar('_');
-	_putchar('\n');
+	else
+	{
+		while (count < n)
+		{
+			_putchar('_');
+			count++;
+		}
+		_putchar('\n');
+	}
 }

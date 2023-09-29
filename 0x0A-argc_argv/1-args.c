a08a30b5e47de90e6642207de1228b5b9d732089
LwandoXabiso <lxb.sigalelana@gmail.com>
2023-09-29 13:55:42 -0700

    hot fix

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
  * main - print the number of arguments
@@ -7,11 +6,10 @@
  * @argv: array of arguments
  * Return: Always 0 (success)
  */
-
 int main(int argc, char *argv[])
 {
 	(void) argv;
-	       printf("%d\n", argc - 1);
-
-	       return (0);
+	printf("%d\n", argc - 1);
+	return (0);
 }
+

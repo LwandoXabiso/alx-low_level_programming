47d72157c836980acc83d934609a9119e31e9a8c
LwandoXabiso <lxb.sigalelana@gmail.com>
2023-10-11 13:46:18 -0700

    function_pointers.h

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stdio.>
+
+/**
+ * print_name - ...
+ * @name: ....
+ * @f: ...
+ * Return: ...
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}

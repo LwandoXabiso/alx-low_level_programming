4bd569fe5b09f6920f89f0ce3f492f487a2b89b0
LwandoXabiso <lxb.sigalelana@gmail.com>
2023-10-24 15:48:16 -0700

    I

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * sum_listint - function that returns the sum of all data
+ * @head: pointer to first node
+ * Return: sum
+ */
+
+int sum_listint(listint_t *head)
+{
+	int add = 0;
+
+	while (head)
+	{
+		add +=head->n;
+		head = head->next;
+	}
+
+	return (add);
+}
